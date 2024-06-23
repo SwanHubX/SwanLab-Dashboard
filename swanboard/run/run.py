adb9435a0c15ad2fbfc69821f987175c74f87d9c
Zirui Cai <1575079435@qq.com>
2024-06-23 15:30:33 +0800

    Update run.py

--- swanboard/run/run.py
+++ swanboard/run/run.py
@@ -71,7 +71,10 @@ class SwanBoardRun:
         start = time.time()
         # debug一下当前日志文件夹的位置
         swl.debug("Try to explore the swanlab experiment logs in: " + FONT.bold(path))
-        connect(path=path)
+        try:
+            connect(path=path)
+        except FileNotFoundError:
+            swl.error("Can not find the swanlab db in: " + FONT.bold(path))
         # ---------------------------------- 日志打印 ----------------------------------
         # 可用URL
         ipv4 = URL.get_all_ip()
