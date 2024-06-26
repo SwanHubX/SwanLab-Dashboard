22096748aa694a8804846aebcefa96ad33926f22
Zirui Cai <1575079435@qq.com>
2024-06-26 15:25:26 +0800

    test: mock run folder

--- test/unit/setup/mock.py
+++ test/unit/setup/mock.py
@@ -1,14 +1,24 @@
-from tutils import mock_project, mock_experiment
+from tutils import mock_project, mock_experiment, mock_folder_with_run_id
+import os
+import nanoid
 
 
-class TestMock:
+def test_mock_project():
+    """在数据库中模拟一个项目记录"""
+    proj_id = mock_project()
+    assert proj_id == 1
 
-    def test_mock_project(self):
-        proj_id = mock_project()
-        assert proj_id == 1
 
-    def test_mock_experiment(self):
-        exp1_id = mock_experiment(1, name="exp1")
-        assert exp1_id == 1
-        exp2_id = mock_experiment(1, name="exp2")
-        assert exp2_id == 2
+def test_mock_experiment():
+    """在数据库中模拟创建实验记录"""
+    mock_project()
+    exp1_id = mock_experiment(1, name="exp1")
+    assert exp1_id == 1
+    exp2_id = mock_experiment(1, name="exp2")
+    assert exp2_id == 2
+
+
+def test_mock_folder_with_run_id():
+    """在测试目录下创建目录"""
+    path = mock_folder_with_run_id(nanoid.generate(size=10))
+    assert os.path.exists(path)
