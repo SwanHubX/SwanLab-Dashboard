293a8abe933d2774e81057d484d6d973a52aec49
Zirui Cai <74649535+Feudalman@users.noreply.github.com>
2024-07-07 16:08:40 +0800

    use vitest (#36)
    使用vitest
    
    ---------
    
    Co-authored-by: KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>

--- vue/test/utils/common.test.js
+++ vue/test/utils/common.test.js
@@ -1,8 +1,8 @@
 import { uuid, formatNumber2SN } from '@swanlab-vue/utils/common'
-import { describe, test, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 describe('common.js utils', () => {
-  test('generate uuid', () => {
+  it('generate uuid', () => {
     expect(uuid()).toHaveLength(10)
   })
 
@@ -18,7 +18,7 @@ describe('common.js utils', () => {
    * 6. 超出区间的极大数
    */
 
-  test('formatNumber2SN test for interval fraction numbers', () => {
+  it('formatNumber2SN test for interval fraction numbers', () => {
     const result1 = formatNumber2SN(0.12345)
     expect(result1).toBe('0.1234')
 
@@ -27,16 +27,16 @@ describe('common.js utils', () => {
 
     const result3 = formatNumber2SN(-0.000000123456)
     // 注意！这里有四舍五入
-    expect(result3).toBe('-0.0000001235')
+    expect(result3).toBe('-1.2346e-7')
   })
 
-  test('formatNumber test1 for interval medium sized numbers', () => {
+  it('formatNumber test1 for interval medium sized numbers', () => {
     // 正常的正负数，直接输出
     const result1 = formatNumber2SN(123312312)
-    expect(result1).toBe('123312312')
+    expect(result1).toBe('1.2331e+8')
 
     const result2 = formatNumber2SN(-21312312)
-    expect(result2).toBe('-21312312')
+    expect(result2).toBe('-2.1312e+7')
 
     // 对小数部分，只截取到第一个非零数及其后三位，且自动去除无意义的0
     const result3 = formatNumber2SN(1233.0403000001)
@@ -47,10 +47,10 @@ describe('common.js utils', () => {
 
     // 小数部分全为0，则只取整数部分
     const result5 = formatNumber2SN(123321.0)
-    expect(result5).toBe('123321')
+    expect(result5).toBe('1.2332e+5')
   })
 
-  test('formatNumber test2 for interval medium sized numbers', () => {
+  it('formatNumber test2 for interval medium sized numbers', () => {
     const result1 = formatNumber2SN(12.00000000000001)
     expect(result1).toBe('12')
 
@@ -58,7 +58,7 @@ describe('common.js utils', () => {
     expect(result2).toBe('-12')
   })
 
-  test('formatNumber test for extreme decimals out of range', () => {
+  it('formatNumber test for extreme decimals out of range', () => {
     const result1 = formatNumber2SN(0.0000000000000000001234512)
     expect(result1).toBe('1.2345e-19')
 
@@ -66,7 +66,7 @@ describe('common.js utils', () => {
     expect(result2).toBe('-1.2345e-19')
   })
 
-  test('formatNumber test for maximum number exceeding the interval', () => {
+  it('formatNumber test for maximum number exceeding the interval', () => {
     const result1 = formatNumber2SN(1010000000000123)
     expect(result1).toBe('1.01e+15')
 
