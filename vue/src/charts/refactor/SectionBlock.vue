e779bb7bc39004d9d676901519bfd0beddd691c6
Zirui Cai <zirui.cai@swanhub.co>
2024-07-13 19:42:08 +0800

    feat: init

---
+++ vue/src/charts/refactor/SectionBlock.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-60 border"></div>
+</template>
+
+<script setup>
+/**
+ * @description: 命名空间相关
+ * @file: SectionBlock.vue
+ * @since: 2024-07-13 18:54:46
+ **/
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue/src/charts/refactor/SectionLayout.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script setup>
+/**
+ * @description: 命名空间布局
+ * @file: SectionLayout.vue
+ * @since: 2024-07-13 17:23:43
+ **/
+</script>
+
+<style lang="scss" scoped></style>
