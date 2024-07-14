babaf0cf49f7e20e842293c4aef7ce1b54e2f596
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-07-14 23:36:42 +0800

    update jsdoc

--- vue/docs/setup.d.js
+++
@@ -1,3 +0,0 @@
-/**
- * @typedef {String} IndexId 唯一id，在前端作为数据查找标识
- */
--- vue/src/board/charts.d.js
+++ vue/src/board/charts.d.js
@@ -1,55 +1,59 @@
 /**
- * @typedef {IndexId} ExperimentId 实验id
+ * @typedef {String} IndexId 在board范围下，唯一id，在前端作为数据查找标识
  */
 
 /**
- * @typedef {IndexId} ColumnKey 指标列的名称
- */
-
-/**
- * @typedef {Object} Section 图表分组
- * @property {'PUBLIC' | 'PINNED' | 'HIDDEN'} type 分组类型 - [PUBLIC, PINNED, HIDDEN]
- * @property {IndexId} index 分组 ID
+ * 图表分组
+ * @typedef {Object} Section
+ * @property {'PUBLIC' | 'PINNED' | 'HIDDEN'} type 分组类型
+ * @property {IndexId} index 分组 ID，唯一识别的 {@link IndexId}
  * @property {String} name 分组名
- * @property {Boolean} pinned 是否固定
  * @property {Boolean} folded 是否折叠
- * @property {Object} config 分组配置
- * @property {IndexId[]} chartIndex 分组内的图表 ID 列表，用于排序
+ * @property {Object} [config] 分组配置
+ * @property {IndexId[]} chartIndex 分组内的图表 ID 列表，用于排序，每个元素为唯一识别的图表 {@link IndexId}
  */
 
 /**
- * @typedef {Object} Chart 图表
+ * 图表
+ * @typedef {Object} Chart
  * @property {IndexId} index 图表 ID
  * @property {String} title 图表名
- * @property {Object} config 图表配置
- * @property {'LINE' | 'TEXT' | 'IMAGE' | 'AUDIO'} type 图表类型 - [LINE, TEXT, IMAGE, AUDIO]
- * @property {Metric[]} metrics 指标 ID 列表，对应 tag 名
+ * @property {Object} [config] 图表配置
+ * @property {'LINE' | 'TEXT' | 'IMAGE' | 'AUDIO'} type 图表类型，分配为不同的组件
+ * @property {Metric[]} metrics 此图表包含的指标列表
  */
 
 /**
- * @typedef {Object} Metric 图表指标配置
- * @property {'X' | 'Y'} axis 指标轴 - [X, Y]
+ * 图表指标配置，需要通过 {@link Column.key} 判断两个指标是否为同一指标，但是即使是同一指标，在不同图表下，配置信息也有可能不同
+ * @typedef {Object} Metric
+ * @property {'X' | 'Y'} axis 指标所在轴
  * @property {String} name 图表中展示的指标名，用于前端显示的指标名称 - 默认情况下，单实验下为指标key，多实验下为实验名
- * @property {ExperimentId} expId 指标对应的实验 ID
- * @property {String[]} colors 指标颜色，用于前端展示，长度为2，第一个为白天模式，第二个为夜间模式
+ * @property {IndexId} expId 指标对应的实验 ID，是唯一识别的 {@link IndexId}
+ * @property {[String, String]} colors 指标颜色，用于前端展示，长度为2，第一个为白天模式，第二个为夜间模式
  * @property {Column} column 列数据，指标实际存储指向
  */
 
 /**
- * @typedef {Object} Column 指标列数据
+ * 指标列数据
+ * @typedef {Object} Column
  * @property {'SYSTEM' | 'CUSTOM'} class 类型 - [SYSTEM, CUSTOM]
- * @property {String | null} error 错误信息, 字符串
- * @property {ColumnKey} key tag key，在项目下唯一，同一个 key 可以出现在多个实验中
+ * @property {String} [error] 错误信息, 字符串
+ * @property {IndexId} key 列的id，每个列通过此属性进行识别，此属性为 列 的唯一识别 {@link IndexId}
  * @property {String} name tag 名称，一般和 key 相同
  * @property {'LINE' | 'TEXT' | 'IMAGE' | 'AUDIO'} type tag 类型 - [LINE, TEXT, IMAGE, AUDIO]
  */
 
 /**
+ * 指标数据超集
+ * @typedef {Object} MetricData
+ * @property {IndexId} experimentId 实验 ID，是唯一识别的 {@link IndexId}
+ * @property {IndexId} key 检索的指标名，是唯一识别的 {@link IndexId}
+ * @property {any[]} metrics 指标数据列表
+ */
+
+/**
  * 指标数据存储对象
- * @typedef {Object} ScalarData
- * @property {ExperimentId} experimentId 实验 ID
- * @property {ColumnKey} key 检索的指标名
- * @property {ScalarDetail[]} metrics 指标数据列表
+ * @typedef {MetricData & {metrics: ScalarDetail[]}} ScalarData
  */
 
 /**
@@ -57,48 +61,72 @@
  * @typedef {Object} ScalarDetail
  * @property {Number} index 步数
  * @property {Number} data 指标数据
- * @property {undefined | true} _last 是否为最后一条数据，最后一个数据设置为true即可，其他不需要设置
+ * @property {true} [_last] 是否为最后一条数据，最后一个数据设置为true即可，其他不需要设置
  */
 
 /**
  * 指标数据存储对象
- * @typedef {Object} MediaData
- * @property {ExperimentId} experimentId 实验 ID
- * @property {ColumnKey} key 指标名
- * @property {Number[]} steps 步数列表，代表这个指标在哪些步数下有数据
- * @property {MediaDetail} metric 最后一步的指标数据
+ * @typedef {MetricData & {metrics: MediaDetail[], steps: Number[]}} MediaData
  */
 
 /**
- * 针对单个指标进行管理
+ * 媒体更多信息存储超集
+ * @typedef {Object} MediaMoreDetail
+ * @property {String} caption 媒体描述
+ */
+
+/**
+ * 针对对于单个指标的详细数据存储结构
  * @typedef {Object} MediaDetail
  * @property {Number} index 步数
  * @property {String[]} data 指标数据
- * @property {Object[] | String[] | undefined} more 指标数据的更多信息，不同类型的数据结构不同，具体结构由图表决定
+ * @property {Array<MediaMoreDetail|null>} [more] 指标数据的更多信息
+ */
+
+// ---------------------------------- 折线图数据结构 ----------------------------------
+
+/**
+ * 折线图图表配置
+ * @typedef { Chart & {type: 'LINE'} } LineChart
  */
 
 /**
- * @typedef {Object<ExperimentId, ScalarData | MediaData>} DataStore 单个指标在不同实验下的数据存储
+ * 折线图数据
+ * @typedef {ScalarData} LineMetricData
+ */
+
+// ---------------------------------- 图像数据结构 ----------------------------------
+
+/**
+ * 图像图表配置
+ * @typedef {Chart & {type: 'IMAGE'} } ImageChart
  */
 
 /**
- * @typedef {Object<ColumnKey, DataStore>} MetricStore 指标信息存储对象
+ * 图像数据
+ * @typedef {MediaData} ImageMetricData
  */
 
-// ---------------------------------- 图表 ----------------------------------
+// ---------------------------------- 音频数据结构 ----------------------------------
 
 /**
- * @typedef {Chart} LineChart
- */
-
-/**
- * @typedef {Chart} ImageChart
- */
-
-/**
- * @typedef {Chart} AudioChart
+ * 音频图表配置
+ * @typedef {Chart & {type: 'AUDIO'} } AudioChart
  */
 
 /**
- * @typedef {Chart} TextChart
+ * 音频数据
+ * @typedef {MediaData} AudioMetricData
+ */
+
+// ---------------------------------- 文字图数据结构 ----------------------------------
+
+/**
+ * 文字图表配置
+ * @typedef {Chart & {type: 'TEXT'} } TextChart
+ */
+
+/**
+ * 文字数据
+ * @typedef {MediaData} TextMetricData
  */
