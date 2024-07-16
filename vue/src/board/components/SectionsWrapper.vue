98a5eef47ffe2ee45c3db0403e6cf80a0f70413e
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-07-16 17:57:09 +0800

    update wrapper

--- vue/src/board/components/SectionsWrapper.vue
+++ vue/src/board/components/SectionsWrapper.vue
@@ -25,9 +25,6 @@ const props = defineProps(['sections', 'charts'])
 
 const emits = defineEmits(['fold-change'])
 
-// 初始化时获取所有已经打开的section index
-const activeSectionIndex = ref(props.sections.filter((section) => !section.folded).map((section) => section.index))
-
 /**
  * 根据section过滤图表，获取到这个section包含的所有图表
  * @param {Section} section section配置
@@ -36,6 +33,10 @@ const filterChartsBySection = (section) => {
   return props.charts.filter((chart) => section.chartIndex.includes(chart.index))
 }
 
+// ---------------------------------- 折叠、展开逻辑 ----------------------------------
+
+// 初始化时获取所有已经打开的section index
+const activeSectionIndex = ref(props.sections.filter((section) => !section.folded).map((section) => section.index))
 /**
  * 当section展开/折叠时触发此事件
  * @param { IndexId[] } activeKey 所有展开的section index
@@ -43,6 +44,7 @@ const filterChartsBySection = (section) => {
 const handleSectionCollapseChange = (activeKey) => {
   console.log('section change', activeKey)
 }
+// ---------------------------------- 拖拽事件委托 ----------------------------------
 
 // TODO 拖拽事件委托
 </script>
