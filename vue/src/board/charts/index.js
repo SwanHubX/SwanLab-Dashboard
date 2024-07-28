fee41330eabf5e01033c75e616d000757d03971e
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-07-28 17:06:41 +0800

    feat:empty chart (#54)
    * feat:empty chart

--- vue/src/board/charts/index.js
+++ vue/src/board/charts/index.js
@@ -2,11 +2,13 @@ import AudioChart from './audio/AudioChart.vue'
 import LineChart from './line/LineChart.vue'
 import ImageChart from './image/ImageChart.vue'
 import TextChart from './text/TextChart.vue'
+import EmptyChart from './empty/EmptyChart.vue'
 
 export default {
   line: LineChart,
   audio: AudioChart,
   image: ImageChart,
   text: TextChart,
-  error: TextChart
+  error: TextChart,
+  empty: EmptyChart
 }
