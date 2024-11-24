7d95a3ca424c1f7bb5ad8c72a867c57eb1abfd98
Zirui Cai <zirui.cai@swanhub.co>
2024-11-24 16:28:47 +0800

    fix: bug

--- vue/src/views/experiment/pages/environment/components/hardware/AppleChip.vue
+++ vue/src/views/experiment/pages/environment/components/hardware/AppleChip.vue
@@ -1,13 +1,13 @@
 <template>
-  <EnvGroups :title="$t('experiment.env.groups.apple')">
+  <div class="w-full py-5 flex-col space-y-5">
+    <h1 class="flex items-center gap-2 font-semibold">{{ $t('experiment.env.groups.apple') }}</h1>
     <EnvItem env-key="brand" :env-value="data.type" />
     <EnvItem env-key="cpu" :env-value="data.cpu" />
     <EnvItem env-key="memory" :env-value="data.memory + 'GB'" />
-  </EnvGroups>
+  </div>
 </template>
 
 <script setup>
-import EnvGroups from '@swanlab-vue/views/experiment/pages/environment/components/EnvGroup.vue'
 import EnvItem from '@swanlab-vue/views/experiment/pages/environment/components/EnvItem.vue'
 
 defineProps({
--- vue/src/views/experiment/pages/environment/pages/EnvHardware.vue
+++ vue/src/views/experiment/pages/environment/pages/EnvHardware.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="w-full border p-6 rounded-lg bg-default" v-if="data">
+  <div class="w-full border p-6 rounded-lg bg-default" v-if="isShow">
     <h1 class="w-full text-xl font-semibold pb-4 border-b">{{ $t('experiment.env.title.hardware') }}</h1>
     <AppleChip :data="data.apple" v-if="isApple" />
-    <EnvGroup :title="$t('experiment.env.groups.cpu')" :data="data.cpu" v-if="data.cpu && !isApple" />
-    <EnvGroup :data="data.memory" v-if="data.memory && !isApple" />
-    <NvidiaGpu :data="data.nvidia" type="nvidia" v-if="data.nvidia" />
-    <NvidiaGpu :data="data.gpu" type="gpu" v-else />
+    <EnvGroup :title="$t('experiment.env.groups.cpu')" :data="data.cpu" v-if="hasCPU && !isApple" />
+    <EnvGroup :data="data.memory" v-if="hasMem && !isApple" />
+    <template v-if="data.gpu">
+      <NvidiaGpu :data="data.nvidia" type="nvidia" v-if="data.nvidia" />
+      <NvidiaGpu :data="data.gpu" type="gpu" v-else />
+    </template>
   </div>
   <div v-else>
     <p class="text-center pt-5">{{ $t('experiment.env.empty.hardware') }}</p>
@@ -29,6 +31,16 @@ const { experiment } = useExperimentStore()
 // 系统硬件信息
 const data = computed(() => getHardwareData(experiment))
 const isApple = computed(() => data.value?.apple)
+// 是否有基础信息
+const hasData = (target) => {
+  if (!target) return false
+  return target?.filter((item) => item.value).length > 0
+}
+const hasCPU = computed(() => hasData(data.value.cpu))
+const hasMem = computed(() => hasData(data.value.memory))
+const isShow = computed(() => {
+  return data.value.apple || hasCPU.value || hasMem.value || data.value.gpu
+})
 </script>
 
 <style lang="scss" scoped></style>
