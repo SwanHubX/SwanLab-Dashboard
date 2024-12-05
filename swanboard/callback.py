73b60614fa32d002e7ef4a4bbc34f4eb61fe5e11
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-12-05 18:25:46 +0800

    delete: config

--- swanboard/callback.py
+++ swanboard/callback.py
@@ -85,7 +85,6 @@ class SwanBoardCallback(SwanKitCallback):
             experiment_id=self.exp,
             type=chart_type,
             reference=column_info.chart_reference.lower(),
-            config=column_info.config,
         )
         # 创建命名空间，如果命名空间已经存在，会抛出ExistedError异常，捕获不处理即可
         # 需要指定sort，default命名空间的sort为0，其他命名空间的sort为None，表示默认添加到最后
