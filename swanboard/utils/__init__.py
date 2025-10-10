a9ba1dd139c37f4cd0c5ca2116b937d9719c9ab2
Zirui Cai <zirui.cai@swanhub.co>
2025-10-10 15:19:59 +0800

    refactor: font

--- swanboard/utils/__init__.py
+++ swanboard/utils/__init__.py
@@ -2,7 +2,7 @@
 工具函数，用于提供一些常用的工具函数，与项目本身无关，在项目任何文件中都可以使用
 """
 
-from .font import generate_color, FONT, COLOR_LIST
+from .font import generate_color, COLOR_LIST
 from .file import check_exp_name_format, check_desc_format
 from .log import swanlog
 from .package import get_package_version
